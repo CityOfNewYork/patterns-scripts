74af5b7f3da54316466b3313db61143c26debde7
devowhippit <devonhirth@gmail.com>
2022-02-17 15:48:06 -0500

    Fix message ARIA toggling in newsletter script

--- src/newsletter/newsletter.js
+++ src/newsletter/newsletter.js
@@ -266,7 +266,7 @@ class Newsletter {
     );
 
     // Screen Readers
-    target.setAttribute('aria-hidden', 'true');
+    target.setAttribute('aria-hidden', 'false');
 
     if (content) {
       content.setAttribute('aria-live', 'polite');
